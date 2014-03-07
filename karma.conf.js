e181709010018d8e158c438f37d5e78691a8d9ec
Vojta Jina <vojta.jina@gmail.com>
2014-03-07 14:48:18 -0800

    chore: use assert from pipe

--- karma.conf.js
+++ karma.conf.js
@@ -12,7 +12,7 @@ module.exports = function(config) {
       {pattern: 'src/**/*.js', included: false},
       {pattern: 'test/**/*.js', included: false},
       {pattern: 'node_modules/di/dist/amd/**/*.js', included: false},
-      {pattern: 'node_modules/assert/dist/amd/**/*.js', included: false},
+      {pattern: 'node_modules/pipe/node_modules/assert/dist/amd/**/*.js', included: false},
       {pattern: 'node_modules/di/node_modules/es6-shim/es6-shim.js', included: false}
     ],
 
