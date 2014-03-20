b7ec314f3b86ea72e1a742a8a787b0334ba0b4cf
Vojta Jina <vojta.jina@gmail.com>
2014-03-20 14:36:11 -0700

    chore: add build_source_cjs

--- gulpfile.js
+++ gulpfile.js
@@ -14,17 +14,27 @@ gulp.task('build_source_amd', function() {
   gulp.src(path.src)
       .pipe(pipe.traceur())
       .pipe(gulp.dest('dist/amd'));
+});
+
+gulp.task('build_examples', function() {
   gulp.src(path.examples)
       .pipe(pipe.traceur())
       .pipe(gulp.dest('temp/examples'));
 });
 
-gulp.task('build', ['build_source_amd']);
+gulp.task('build_source_cjs', function() {
+  gulp.src(path.src)
+      .pipe(pipe.traceur({modules: 'commonjs'}))
+      .pipe(gulp.dest('dist/cjs'));
+});
+
+gulp.task('build', ['build_source_amd', 'build_source_cjs', 'build_examples']);
 
 
 // WATCH FILES FOR CHANGES
 gulp.task('watch', function() {
-  gulp.watch([path.src, path.examples], ['build']);
+  gulp.watch([path.src], ['build_source_amd']);
+  gulp.watch([path.examples], ['build_examples']);
 });
 
 
