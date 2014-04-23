ed03c693f78d2080f2102bdfcd9c1de031cb69d1
Vojta Jina <vojta.jina@gmail.com>
2014-04-22 17:22:43 -0700

    chore(travis): use SL for helloworld e2e test

--- examples/protractor.conf.js
+++ examples/protractor.conf.js
@@ -1,20 +1,32 @@
-// An example configuration file.
-exports.config = {
-  // The address of a running selenium server.
+var localConfig = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
-
-  // Capabilities to be passed to the webdriver instance.
   capabilities: {
     'browserName': 'chrome'
   },
-
-  // Spec patterns are relative to the current working directly when
-  // protractor is called.
   specs: ['test.js'],
-
-  // Options to be passed to Jasmine-node.
   jasmineNodeOpts: {
     showColors: true,
     defaultTimeoutInterval: 30000
   }
 };
+
+var travisWithSauceConfig = {
+  capabilities: {
+    'browserName': 'chrome',
+    'version': '33',
+    'platform': 'OS X 10.9',
+    'name': 'Angular Templating',
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    'build': process.env.TRAVIS_BUILD_NUMBER,
+    'chromedriver-version': 'https://raw.githubusercontent.com/angular/templating/custom-chromedriver-on-sauce/chromedriver/chromedriver_mac.zip'
+  },
+  sauceUser: process.env.SAUCE_USERNAME,
+  sauceKey: process.env.SAUCE_ACCESS_KEY.split('').reverse().join(''),
+  specs: ['test.js'],
+  jasmineNodeOpts: {
+    showColors: true,
+    defaultTimeoutInterval: 30000
+  }
+};
+
+exports.config = process.env.TRAVIS ? travisWithSauceConfig : localConfig;
